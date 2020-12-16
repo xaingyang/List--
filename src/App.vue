5b6e40791500a0ed6be9ada42a41725214aa73e5
xiangyang <1256599321@qq.com>
2020-12-16 18:58:05 +0800

    简单地发送axios请求

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,22 @@
 <template>
-  <div class="todo-container">
-    <div class="todo-wrap">
-      <!-- <Header :addTodo="addTodo"/> -->
-      <Header @addTodo="addTodo"/>
-      <List :todos="todos" :updateOne="updateOne" :deleteOne="deleteOne"/>
-      <Footer :todos="todos" :AllCheck="AllCheck" :deleteAll="deleteAll"/>
-    </div>
+  <div class="container">
+      <Header/>
+      <Main/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
-import Footer from './components/Footer'
-import List from './components/List'
-
+import Main from './components/Main'
 export default {
   name: '',
   components:{
-    Header,
-    Footer,
-    List
-  },
-  data() {
-    return {
-      // todos:[
-      //   {id:1,content:'追剧',isOver:false},
-      //   {id:2,content:'打球',isOver:true},
-      //   {id:3,content:'睡觉',isOver:false},
-      //   {id:4,content:'喝酸奶',isOver:true},
-      // ]
-      todos:JSON.parse(localStorage.getItem('TODOS_KEY')) || []
-    }
-  },
-  watch:{
-      todos:{
-        deep:true,
-        handler(newVal,oldVal){
-          localStorage.setItem('TODOS_KEY',JSON.stringify(newVal))
-        }
-      }
-  },
-  methods: {
-    addTodo(todo){
-      return this.todos.unshift(todo)
-    },
-    updateOne(index){
-      this.todos[index].isOver=!this.todos[index].isOver
-    },
-    deleteOne(index){
-      this.todos.splice(index,1)
-    },
-    AllCheck(val){
-      this.todos.forEach(item=>item.isOver=val)
-    },
-    deleteAll(){
-      this.todos=this.todos.filter(item=>!item.isOver)
-    }
-  },
+      Header,
+      Main
+  }
 }
 </script>
 
 <style scoped>
-/*app*/
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
 
-/*header*/
-.todo-header input {
-  width: 560px;
-  height: 28px;
-  font-size: 14px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 4px 7px;
-}
-
-.todo-header input:focus {
-  outline: none;
-  border-color: rgba(82, 168, 236, 0.8);
-  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
-}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,61 +1,36 @@
 <template>
-  <div class="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认" v-model="content" @keyup.enter="addT"/>
-  </div>
+  <section class="jumbotron">
+      <h3 class="jumbotron-heading">搜索github的用户</h3>
+      <div>
+        <input type="text" placeholder="请输入你要搜索的内容" v-model="keyword"/>
+        <button @click="search">搜索</button>
+      </div>
+    </section>
 </template>
 
 <script>
 export default {
   name: '',
-  data(){
+  data() {
       return {
-          content:''
+          keyword:''
       }
   },
-//   props:{
-//       addTodo:{
-//           type:Function,
-//           required:true
-//         //   type:Number,
-//         //   required:true,  //代表必须传值
-//         //   default:10      //代表如果没有传值，就默认值为10
-//       }
-//   },
   methods:{
-      addT(){
-          let {content}=this
-          if(content.trim()){
-              let id=Date.now()
-              let isOver=false
-              let obj={
-                  id,
-                  content,
-                  isOver
-              }
-            //   this.addTodo(obj)
-            this.$emit('addTodo',obj)
+      search(){
+          let keyword=this.keyword
+          if(keyword.trim()){
+               this.$bus.$emit('searchAjax',keyword)
           }else{
-              alert('请输入合法的内容')
+              alert('关键字格式不合法')
           }
+
+          this.keyword=""
       }
   }
 }
 </script>
 
 <style scoped>
-/*header*/
-.todo-header input {
-  width: 560px;
-  height: 28px;
-  font-size: 14px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 4px 7px;
-}
 
-.todo-header input:focus {
-  outline: none;
-  border-color: rgba(82, 168, 236, 0.8);
-  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
-}
 </style>
